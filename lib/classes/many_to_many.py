d9661c412214e95b88b2f5f1a79c3eeeb7171128
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-21 15:48:33 +0300

    added object relationship methods and properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,12 @@
 class Article:
+    
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self._title = title
+        Article.all.append(self)
     
     @property
     def title(self):
@@ -13,12 +17,25 @@ class Article:
         if title != self.title:
             return self._title
         elif isinstance(title, str) and 5 <= len(title) <= 50:
-            self._title = title
+            self._title = title   
     
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, Author):
+            raise Exception
+        self._author = author
         
 class Author:
+
+    all = []
+
     def __init__(self, name):
         self._name = name
+        Author.all.append(self)
     
     @property
     def name(self):
@@ -32,21 +49,29 @@ class Author:
             self._name = name
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        unique_magazines = set([article.magazine for article in self.articles()])
+        return list(unique_magazines)
 
     def add_article(self, magazine, title):
         pass
+        # if not isinstance(magazine, Magazine):
+        #     raise Exception
+        # return Article(self, magazine, title)
 
     def topic_areas(self):
         pass
 
 class Magazine:
+
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
     
     @property
     def name(self):
@@ -67,13 +92,16 @@ class Magazine:
             self._category = category
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        unique_contributors = set([article.author for article in self.articles()])
+        return list(unique_contributors)
 
     def article_titles(self):
-        pass
+        if [article.title for article in self.articles()] == []:
+            return None
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
         pass
